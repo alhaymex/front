baa4bd94e52d86fb6fafcc2fb236bc2882ac1817
alhaymex <alhaymex@gmail.com>
2021-10-15 23:41:25 +0200

    Even more Structuring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+const App = () => {
+  return <React.Fragment></React.Fragment>;
+};
 
 export default App;
